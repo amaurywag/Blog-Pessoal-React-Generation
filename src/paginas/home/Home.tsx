f29b0c8f73049cdebef5d3591a1aca129c4b5567
Amaury <86973979+amaurywag@users.noreply.github.com>
2022-03-22 16:57:32 -0300

    Atividade React 2 - Plataforma Generation

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <> 
+            <h1 className='titulo'>Home</h1>
+            <img src="https://img.cybercook.com.br/receitas/75/cuscuz-com-leite-de-coco.jpeg" alt="Imagem de cuscuz com leite" className='img'  />
+        </>
+    );
+}
+
+export default Home;
