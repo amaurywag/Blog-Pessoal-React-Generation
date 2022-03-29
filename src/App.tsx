b19a760233221314b0ad9ae6aa7eab3c21aa21c4
Amaury <86973979+amaurywag@users.noreply.github.com>
2022-03-29 18:42:21 -0300

    Atividade React 3 - Plataforma Generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Home from './paginas/home/Home';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
 
 function App() {
   return (
-    <Home />
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
   );
 }
 
